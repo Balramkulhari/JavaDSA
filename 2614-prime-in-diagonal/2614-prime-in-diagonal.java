a5228f5897ff50aff3a0aea71c1cd5ffcae88525
Balramkulhari <146817802+Balramkulhari@users.noreply.github.com>
2026-07-31 14:50:28 +0530

    Time: 5 ms (60.8%), Space: 82.1 MB (95.42%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.java
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -1,30 +1,20 @@
 class Solution {
-    public int diagonalPrime(int[][] matrix) {
-        int n = matrix.length;
-        int lp = 0;
-
-        // Primary diagonal
-        for (int i = 0; i < n; i++) {
-            if (isPrime(matrix[i][i])) {
-                lp = Math.max(lp, matrix[i][i]);
-            }
-        }
-
-        // Secondary diagonal
-        for (int i = 0; i < n; i++) {
-            if (i != n - 1 - i && isPrime(matrix[i][n - 1 - i])) {
-                lp = Math.max(lp, matrix[i][n - 1 - i]);
-            }
-        }
-
-        return lp;
-    }
-
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) return false;
+    boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
         }
         return true;
+
+    }
+    public int diagonalPrime(int[][] nums) {
+        int n = nums.length;
+        int m = 0;
+        //checking primary
+        for (int i = 0; i < n; i++) {
+            if (isPrime(nums[i][i])) m = Math.max(nums[i][i], m);
+            if (isPrime(nums[i][n - 1 - i])) m = Math.max(nums[i][n - 1 - i], m);
+        }
+        return m;
     }
 }
