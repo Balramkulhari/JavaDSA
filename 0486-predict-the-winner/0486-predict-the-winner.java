98a19b17d841847e2e63d618c5d15a996e4ec2b7
Balramkulhari <146817802+Balramkulhari@users.noreply.github.com>
2026-08-01 12:08:58 +0530

    Time: 0 ms (100%), Space: 42.3 MB (97.59%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n =  nums.length;
+        Integer[][] dp = new Integer[n][n];
+        boolean m =  solve(nums,0,n-1 ,dp)>=0;
+        return m;
+        
+    }
+    private int solve(int[] nums , int i , int j ,Integer[][] dp){
+        if(i>j) return 0;
+        if(dp[i][j]!=null) return dp[i][j];
+        if(i==j ) return dp[i][j]=nums[i];
+
+        int take_i = nums[i] - solve(nums,i+1,j,dp);
+        int take_j = nums[j] - solve(nums,i,j-1,dp);
+        return dp[i][j]=Math.max(take_i , take_j);
+    }
+}
