8e9882467c4f7e35dda21d7f6264b15956e0266c
Balramkulhari <146817802+Balramkulhari@users.noreply.github.com>
2026-08-02 11:01:18 +0530

    Time: 1 ms (25.19%), Space: 46.7 MB (19.1%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,18 +1,28 @@
 class Solution {
-    public boolean stoneGame(int[] nums) {
-       int n =  nums.length;
-        Integer[][] dp = new Integer[n][n];
-        boolean m =  solve(nums,0,n-1 ,dp)>=0;
-        return m;
-        
-    }
-    private int solve(int[] nums , int i , int j ,Integer[][] dp){
-        if(i>j) return 0;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(i==j ) return dp[i][j]=nums[i];
+    public boolean stoneGame(int[] piles) {
+        Integer dp[][] = new Integer[piles.length][piles.length];
 
-        int take_i = nums[i] - solve(nums,i+1,j,dp);
-        int take_j = nums[j] - solve(nums,i,j-1,dp);
-        return dp[i][j]=Math.max(take_i , take_j);
-    } 
+        // return solve(piles, 0, piles.length-1,dp) >0;
+        return true;
+    }
+
+    public int solve(int[] nums, int left,int right, Integer[][] dp){
+
+        if(left==right){
+            return nums[left];
+        }
+
+        if(dp[left][right]!=null){
+            return dp[left][right];
+        }
+
+        int leftAns = nums[left] - solve(nums, left+1,right, dp );
+        int rightAns = nums[right] - solve(nums, left, right-1, dp);
+
+        dp[left][right] = Math.max(leftAns, rightAns);
+
+        return dp[left][right];
+
+    }
+
 }
